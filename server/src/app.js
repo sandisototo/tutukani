b2f664a45b489950eba8b63711918d2a374f9f4c
Anthony Galagade <galagadea@gmail.com>
2018-08-07 21:19:05 +0200

    update bodyparser

--- server/src/app.js
+++ server/src/app.js
@@ -8,8 +8,8 @@ const config = require('./config/config')
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
-app.use(cors())
-
+app.use('*',cors())
+app.use(bodyParser.urlencoded({extended: true}));
 require('./passport')
 
 require('./routes')(app)
